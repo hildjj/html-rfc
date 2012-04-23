302dac6fdabb97a1b0b557042260c24f6d0c9e91
Joe Hildebrand <joe-github@cursive.net>
2012-04-23 16:39:51 -0600

    Make paragraph numbering more triumphant.  Apply the nits to the draft.

--- nits/p-number.js
+++ nits/p-number.js
@@ -1,12 +1,15 @@
-exports.nit = function(env) {
+function pnum_nit(env) {
     var $ = env.$;
     var i = 0;
     $("p").each(function() {
         var t = $(this);
         var id = t.attr("id");
-        if (!id || id.match(/^para-\d+$/)) {
-            t.attr("id", "para-" + i);
+        if (!id || id.match(/-p-\d+$/)) {
+            var divid = t.closest("div").attr("id");
+            t.attr("id", divid + "-p-" + t.index());
             i++;
         }
     });
-};
+}
+pnum_nit.requires = "div-number.js";
+exports.nit = pnum_nit;
