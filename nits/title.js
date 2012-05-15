4951430ed874534c0bc2c11bd125fc72616e5f17
Joe Hildebrand <joe-github@cursive.net>
2012-05-15 10:03:40 -0600

    Create a title if one doesn't exist.

--- nits/title.js
+++ nits/title.js
@@ -1,13 +1,26 @@
+var path = require('path');
+
 exports.nit = function(env) {
     var $ = env.$;
     // Title is required
     var h1 = $("div#title h1");
-    if (h1.length != 1) {
-        return env.error("Wrong number of title divs: %d", h1.length);
+    if (h1.length === 0) {
+        env.log.warn("No Title.  Adding one that needs to be edited.");
+        $("div.ietf").after($("<div id='title'><h1>Template</h1></div>"));
+        h1 = $("div#title h1");
     }
     var txt = h1.text();
     $("html > head > title").text(txt);
     $("html > head > name[description]").attr('content', txt);
+
+    if (env.argv.outfile) {
+        var fn = $("div#title > div#filename")
+        if (fn.length === 0) {
+            fn = $("<div id='filename' />");
+            h1.after(fn);
+        }
+        fn.text(path.basename(env.argv.outfile));
+    }
 }
 
-exports.requires = "header.js";
+exports.requires = ["header.js", "authors.js"];
