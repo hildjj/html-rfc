93b40a641d23f1939ea8f3892c740b51f4cb7fb6
Joe Hildebrand <joe-github@cursive.net>
2012-06-06 21:06:41 -0600

    Automatically add the mustard text to the draft.

--- nits/2119.js
+++ nits/2119.js
@@ -10,11 +10,17 @@ exports.nit = function(env) {
         }
     });
 
+    var m = $("div#mustard");
+    if (m.text().trim().length === 0) {
+        $("<h3>Terminology</h3>").appendTo(m);
+        $("<p>The key words \"MUST\", \"MUST NOT\", \"REQUIRED\", \"SHALL\", \"SHALL NOT\", \"SHOULD\", \"SHOULD NOT\", \"RECOMMENDED\", \"NOT RECOMMENDED\", \"MAY\", and \"OPTIONAL\" in this document are to be interpreted as described in <a class='ref' href='#rfc:2119'>RFC 2119</a>.</p>").appendTo(m);
+    }
+
     var mustard = /((MUST|SHOULD|MAY|SHALL|OPTIONAL|REQUIRED|(NOT\s+)?RECOMMENDED)(\s+NOT)?)/g;
     $('body').find('*').textNodes().each(function(i){
         var p = this.parentNode;
         var pname = p.nodeName.toLowerCase();
-        if ((p.className === "rfc2119") || (pname === 'pre') || (pname === 'code')) {
+        if ((p.className === 'rfc2119') || (pname === 'pre') || (pname === 'code')) {
             return;
         }
         var found = false;
