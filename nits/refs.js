766b4094dd22b2876f01dd67cd16b8c945c6bbc3
Joe Hildebrand <joe-github@cursive.net>
2012-07-16 16:22:25 -0600

    Change the way refs are generated.  Still needs work.

--- nits/refs.js
+++ nits/refs.js
@@ -16,6 +16,48 @@ var refmap = {
     "other" : "{id}"
 };
 
+function surname(author) {
+    var sur = $('<span>').addClass('surname');
+    var mailto = $("email", author).text();
+    var org = $("organization", author).text();
+
+    if (mailto || org) {
+        var a = $('<a>');
+        if (mailto) {
+            a.attr('href', 'mailto:' + mailto)
+        }
+        if (org) {
+            a.attr('title', org);
+        }
+        a.text(author.attr('surname')).appendTo(sur);
+    } else {
+        sur.append(author.attr('surname'));
+    }
+    return sur;
+}
+
+function author(author, first) {
+    var a = $(author);
+    var auth = $('<span>').addClass('author');
+    var initials = $('<span>').addClass('initials').text(a.attr('initials'));
+
+    if (first) {
+        auth.append(surname(a));
+        auth.append(", ");
+        auth.append(initials);
+    } else {
+        auth.append(initials);
+        auth.append(" ");
+        auth.append(surname(a));
+    }
+
+    if (a.attr('role') === 'editor') {
+        author.append(", ");
+        $("<span>").addClass('role').text("Ed.").appendTo(auth);
+    }
+    return auth;
+}
+
 exports.nit = function(env) {
     var $ = env.$;
     var prom = [];
@@ -24,7 +66,7 @@ exports.nit = function(env) {
     // TODO: support div.inforef
 
     // all references that haven't been filled in
-    $("li.ref:empty").each(function() {
+    $("#references li:empty").each(function() {
         var ref = $(this);
         var id = ref.attr("id");
 
@@ -38,7 +80,7 @@ exports.nit = function(env) {
         var series = matches[1].toLowerCase();
         var u = refmap[series];
         if (!u) {
-            return env.error("bad series");
+            return env.error("bad series: " + series);
         }
         id = matches[2];
         // RFC's and XEP's are 4 digits
@@ -50,32 +92,67 @@ exports.nit = function(env) {
         env.log.info("Getting", series, id, "(" + u + ")");
 
         prom.push($.get(u)
-                        .done(function(data){
-                            var anchor = $('reference', data).attr('anchor');
-                            $('<span>').attr('id', anchor).text("[" + anchor + "]").appendTo(ref);
-                            var author = $('<span>').addClass('author').appendTo(ref);
-                            var nm = $('author', data).attr('surname') + ", " + $('author', data).attr('initials');
-                            var mailto = $("author email", data).text();
-                            var org = $("author organization", data).text();
-                            if (mailto || org) {
-                                $('<a>')
-                                    .attr('href', 'mailto:' + $("author email", data).text())
-                                    .attr('title', $("author organization", data).text())
-                                    .text(nm)
-                                    .appendTo(author);
-                            } else {
-                                author.append(nm);
+                        .done(function(data, status, x) {
+                            if (status !== "success") {
+                                return;
                             }
-                            ref.append(", ");
-                            $('<span>').addClass('title').text('"' + $('title', data).text() + '"')
-                                .appendTo(ref);
 
+                            ref.empty();
+                            if (!env.argv['final']) {
+                                var h = x.responseText.replace(/^<\?xml version='1\.0' encoding='UTF-8'\?>\s*/, "");
+                                ref.attr('data-xml', h);
+                                //var h = $("<p>").append(data.eq(0).clone()).html();
+                                //env.log.debug(h);
+                                //ref.attr('data-xml', h);
+                            }
+                            var anchor = $('reference', data).attr('anchor');
+                            ref.append("[")
+                            $('<span>').attr('id', anchor).text(anchor).appendTo(ref);
+                            ref.append("] ");
+                            var authors = $('author', data);
+                            switch (authors.length) {
+                                case 0:
+                                    // some sort of fouled up input
+                                    env.log.warn("No authors in reference: " + anchor);
+                                    return;
+                                case 1:
+                                    author(authors[0], true).appendTo(ref);
+                                    break;
+                                case 2:
+                                    // Preston, W. and T. Logan
+                                    author(authors[0], true).appendTo(ref);
+                                    ref.append(" and ")
+                                    author(authors[1], false).appendTo(ref);
+                                    break;
+                                default:
+                                    // Howard, M., C. Howard, and L. Fine
+                                    author(authors[0], true).appendTo(ref);
+                                    for (var i=1; i<authors.length; i++) {
+                                        ref.append(", ");
+                                        if (i == (author.length-1)) {
+                                            ref.append("and ");
+                                        }
+                                        author(authors[i], false).appendTo(ref);
+                                    }
+                                    break;
+                            }
+                            ref.append(', "');
+                            $('<span>').addClass('title').text($('title', data).text())
+                                .appendTo(ref);
+                            ref.append('", ');
                             var reflinks = $('<span>').addClass('reflinks').appendTo(ref);
                             $('format', data).each(function() {
                                 var fmt = $(this);
+                                var af = $('<a>')
+                                    .attr('href', fmt.attr('target'))
+                                    .text(fmt.attr('type'));
+                                var octets = fmt.attr('octets');
+                                if (octets) {
+                                    af.attr('title',  octets + " octets");
+                                }
                                 $('<span>')
-                                    .append('[')
-                                    .append($('<a>').attr('href', fmt.attr('target')).text(fmt.attr('type')))
+                                    .append(' [')
+                                    .append(af)
                                     .append(']')
                                     .appendTo(reflinks);
                             });
