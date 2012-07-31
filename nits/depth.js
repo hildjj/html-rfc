1b85f9ac9e31dd49ff75782bcf918742a12a7de5
Joe Hildebrand <joe-github@cursive.net>
2012-07-30 19:13:52 -0700

    Max depth of 6.

--- nits/depth.js
+++ nits/depth.js
@@ -3,6 +3,9 @@ exports.nit = function(env) {
     $('.section h2,h3,h4,h5,h6').each(function() {
         var h = $(this);
         var depth = h.parentsUntil("body").length + 1;
+        if (depth > 6) {
+            depth = 6;
+        }
         if (this.name.toUpperCase() !== "H"+depth) {
             env.log.warn("Bad depth: ", h.parent().attr("id"));
             h.after($("<h"+depth+">" + h.text() + "</h"+depth+">"));
